e8a4633735bdbc3e0823a6030fcf7683bf3981b9
Xelanor <berozel@hotmail.com>
2019-10-28 11:40:00 +0300

    Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        clone: '#E785FF',
+        clone2: '#282828',
+        clone3: '#C4C4C4',
+        clone4: '#B5B3B3',
+        clone5: '#545454'
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
